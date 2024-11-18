acc05cf97c332bae396505bcbc4f86801084ad0e
cecilia <yijialiu523@gmail.com>
2024-11-18 18:50:15 -0500

    update

--- first_app/src/index.js
+++ first_app/src/index.js
@@ -2,14 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Contact from './Components/Contact'
 import ProjectCard from './Components/ProjectCard';
+import ProjectDetails from './Components/ProjectDetails'
 import projectImage1 from './assets/project1.jpg'; 
 import projectImage2 from './assets/project2.jpg'; 
+import projectImage3 from './assets/project3.jpg'; 
+import projects from './projects.json';
 
 const element = <h1>Hello world</h1>;
+const projectImages = [projectImage1, projectImage2, projectImage3]; // Array for mapping images
+const firstProject = projects[0];
 
 const app = (
     <>
-        <ProjectCard
+        {/* const projectImages = [projectImage1, projectImage2, projectImage3]; // Array for mapping images */}
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', justifyContent: 'center' }}>
+        {/* <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', justifyContent: 'center' }}> */}
+            {projects.map((project, index) => (
+                <ProjectCard
+                    key={project.id} // Unique key for React
+                    image={projectImages[index]} // Dynamically assign images
+                    title={project.title}
+                    description={project.description}
+                    link={project.link}
+                    skill={project.skill}
+                />
+            ))}
+        </div>
+        {/* <ProjectCard
                 image={projectImage1}
                 title="The First Project"
                 description="A brief overview of the second project."
@@ -20,8 +39,15 @@ const app = (
                 title="The Second Project"
                 description="A brief overview of the second project."
                 // link="https://example.com/project1" // Link to the project page
-        />
+        /> */}
         <Contact />
+        <ProjectDetails
+            title={firstProject.title}
+            image={require(`${firstProject.image}`)}
+            description={firstProject.description}
+            skill={firstProject.skill}
+            objectives={firstProject.objectives}
+        />
     </>
 );
 
